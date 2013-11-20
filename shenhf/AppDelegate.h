18832ae3fa83a5e99e0bf8c058ba9fc20a04cc3c
echo <echozhjun@gmail.com>
2013-11-20 15:39:48 +0800

    init
    init submit

---
+++ shenhf/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  shenhf
+//
+//  Created by echo on 13-11-20.
+//  Copyright (c) 2013年 shenhf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ shenhf/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  shenhf
+//
+//  Created by echo on 13-11-20.
+//  Copyright (c) 2013年 shenhf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
